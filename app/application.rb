f4854c0179bb18abd98b97458fbfba9807ec1f23
Gregory Losco <gtlosco@gmail.com>
2019-09-16 22:43:55 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -11,6 +11,8 @@ class Application
       item = @@items.find{|i| i.name == item_name}
       
       resp.write item.price
+    else 
+      resp.write "Route not found"
     end
       resp.finish
   end
