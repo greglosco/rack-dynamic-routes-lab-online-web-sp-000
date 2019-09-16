2ab02fb6d673d6d08bd94a9f5156681fabb85348
Gregory Losco <gtlosco@gmail.com>
2019-09-16 22:38:48 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,7 @@ class Application
     
     if req.path.match (/items/)
       item_name = req.path.split("/items").last 
-      item = @@items.find{|i| item.
+      item = @@items.find{|i| item.name == item_name}
   end
   
 end
